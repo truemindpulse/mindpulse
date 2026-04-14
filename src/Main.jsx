72051d53002d51acd6caf8d5641cc39e115655e4
truemindpulse <truemindpulse@gmail.com>
2026-04-14 06:14:16 -0400

    Delete src/Main.jsx

--- src/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from “react”;
-import ReactDOM from “react-dom/client”;
-import MindPulse from “./MindPulse”;
-
-ReactDOM.createRoot(document.getElementById(“root”)).render(
-<React.StrictMode>
-<MindPulse />
-</React.StrictMode>
-);
